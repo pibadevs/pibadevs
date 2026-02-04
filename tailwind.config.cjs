c33f171f7f37623f96a246590f989a4825cb893a
ozark7 <79755201+ozark7@users.noreply.github.com>
2026-02-04 18:34:02 -0500

    chore: update styles and improve component hover effects

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+        quaternary: "var(--color-quaternary)",
+        quinary: "var(--color-quinary)",
+      },
+    },
   },
   plugins: [],
 }
